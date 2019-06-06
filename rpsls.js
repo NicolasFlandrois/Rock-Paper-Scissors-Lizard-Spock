c4f48304b4284577bdd294149a7ca513bb7ec173
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-06 23:56:27 +0200

    JS Cashing the Dumb
    stating all constant variables, storing it right at beginning, and ready to use in the following scripts.

---
+++ rpsls.js
@@ -0,0 +1,12 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rck");
+const paper_div = document.getElementById("ppr");
+const scissors_div = document.getElementById("ssr");
+const lizard_div = document.getElementById("lzr");
+const spock_div = document.getElementById("spk");
+
